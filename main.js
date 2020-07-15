dece87c2e21a93b5f7f8b8a53ddb756aead9f6ea
Nick Mykhailyshyn <whoareme@wearehackerone.com>
2020-07-15 18:39:50 +0300

    Added: arguments parser
    Added: URL validator

--- main.js
+++ main.js
@@ -1,49 +1,74 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs').promises;
 const parser = require('heapsnapshot-parser');
-const url = process.argv[2];
+const {url} = require('yargs')
+    .option('url', {
+        alias: 'u',
+        type: 'string',
+        describe: 'url address'
+    })
+    .demandOption(['url'])
+    .help()
+    .argv
 
-if (url != undefined) {
-  init(url);
+
+init(new URL(url));
+
+
+async function getHeapSnapshot(page, browser) {
+    const client = await page.target().createCDPSession();
+    const chunks = [];
+
+    client.on('HeapProfiler.addHeapSnapshotChunk', ({chunk}) => {
+        chunks.push(chunk);
+    });
+
+    await client.send('HeapProfiler.takeHeapSnapshot', {reportProgress: false});
+    await browser.close();
+
+    const snapshot = parser.parse(chunks.join(''));
+
+    return snapshot;
+}
+
+function parsePathsFromSnapshot(snapshot) {
+    const paths = new Set()
+    const regex = /[^/]([\/][a-zA-Z0-9_.-]+)+(?!([gimuy]*[,;\s])|\/\2)/g;
+
+    for (let i = 0; i < snapshot.nodes.length; i++) {
+        const node = snapshot.nodes[i];
+        const search = node.name.match(regex);
+
+        if (search != null) {
+            search.forEach((path) => {
+                paths.add(path.substr(1))
+            })
+        }
+    }
+
+    return paths;
+}
+
+function outputResult(data) {
+    const sortedData = Array.from(data).sort();
+
+    sortedData.forEach((path) => {
+        console.log(path)
+    })
 }
 
 async function init(url) {
-  const filename = '/tmp/heap.heapsnapshot';
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
 
-  await page.goto(url);
+        await page.goto(url, {timeout: 12000});
 
-  const client = await page.target().createCDPSession();
-  const chunks = [];
+        const snapshot = await getHeapSnapshot(page, browser);
+        const paths = parsePathsFromSnapshot(snapshot);
 
-  client.on('HeapProfiler.addHeapSnapshotChunk', ({ chunk }) => {
-    chunks.push(chunk);
-  });
-
-  await client.send('HeapProfiler.takeHeapSnapshot', { reportProgress: false });
-  await fs.writeFile(filename, chunks.join(''));
-  await browser.close();
-
-  const snapshotFile = await fs.readFile(filename, {encoding: "utf-8"});
-  const snapshot = parser.parse(snapshotFile);
-
-  var paths = new Set()
-  var regex = /[^/]([\/][a-zA-Z0-9_.-]+)+(?!([gimuy]*[,;\s])|\/\2)/g;
-
-  for (let i = 0; i < snapshot.nodes.length; i++) {
-    var node = snapshot.nodes[i];
-    var search = node.name.match(regex);
-
-
-    if (search != null) {
-      search.forEach((path) => {
-        paths.add(path.substr(1))
-      })
+        outputResult(paths);
+    } catch (error) {
+        console.error('Unable to parse paths on this URL. Error message: ', error);
+        process.exit(-1);
     }
-  }
-
-  [...paths].forEach((path) => {
-    console.log(path)
-  })
 }
